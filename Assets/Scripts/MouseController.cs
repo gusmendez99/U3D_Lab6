35c721fd4c7e8e3af0f9dbcd85e863632e13c015
gusmendez99 <gus.mendez.99@gmail.com>
2019-05-04 12:09:41 -0600

    NavMesh Added, soldier moves well on surface

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouseController : MonoBehaviour
+{
+
+    private Animator mAnimator;
+    private NavMeshAgent mNavMeshAgent;
+    private bool mRunning;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+        mNavMeshAgent = GetComponent<NavMeshAgent>();
+        mRunning = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                mNavMeshAgent.destination = hit.point;
+                              
+            }
+        }
+
+        if (mNavMeshAgent.remainingDistance <= mNavMeshAgent.stoppingDistance)
+        {
+            mRunning = false;
+        }
+        else
+        {
+            mRunning = true;
+     
+        }
+
+        mAnimator.SetBool("running", mRunning);
+    }
+}
